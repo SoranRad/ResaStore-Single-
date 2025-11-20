7d812811d98abf8d80238dff178a2834b9b86c5e
Soran Rad <twenty.devs@gmail.com>
2025-11-20 09:39:05 +0330

    ریست کردن تنظیمات در General DataLayer

--- General/NZ.General.DataLayer/Migrations/202511200602104_InitialCreate.Designer.cs
+++ General/NZ.General.DataLayer/Migrations/202511200602104_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace NZ.General.DataLayer.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class adduserlock : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(adduserlock));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "202506290614099_add-user-lock"; }
+            get { return "202511200602104_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
